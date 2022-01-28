f9566b841563ef3dc552c65fd90acc20ad04bf52
Belov Roman <rnbelov96@gmail.com>
2022-01-28 14:32:43 +0300

    Make director edits

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ import './ts/calc';
 import './ts/setup-forms';
 import './ts/f5leads-init';
 import './ts/f5leads';
+import './ts/year';
+import './ts/animation';
--- src/ts/thanks.ts
+++ src/ts/thanks.ts
@@ -5,7 +5,7 @@ const userName = localStorage.getItem('userName');
 
 const nameLabelEl = document.querySelector('.js-name') as HTMLSpanElement;
 nameLabelEl.textContent = userName
-  ? `${localStorage.getItem('userName')}`
-  : 'Гость';
+  ? `${localStorage.getItem('userName')},`
+  : 'Гость,';
 
 document.title = userName ? `${userName}, спасибо, Ваша заявка принята` : 'Cпасибо, Ваша заявка принята';
